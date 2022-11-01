50eb20189b008d2a6ef6c9c36aa62dab3b9cb943
Ronaldo <ronaldovctor@outlook.com>
2022-11-01 19:03:18 -0300

    app: layout created

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -6,6 +6,28 @@ module.exports = {
 			fontFamily: {
 				sans: 'Roboto, sans-serif',
 			},
+
+			backgroundImage: {
+				app: 'url(/app-bg.png)',
+			},
+
+			colors: {
+				ignite: {
+					500: '#129E57',
+				},
+				gray: {
+					900: '#121214',
+					800: '#202014',
+					600: '#323238',
+					300: '#8D8D99',
+					100: '#E1E1E6',
+				},
+
+				yellow: {
+					500: '#F7DD43',
+					700: '#E5CD3D',
+				},
+			},
 		},
 	},
 	plugins: [],

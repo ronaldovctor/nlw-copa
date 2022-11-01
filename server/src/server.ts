452030230a97366188f80213a9703495c967750f
Ronaldo <ronaldovctor@outlook.com>
2022-11-01 13:50:48 -0300

    api: added zod lib to validate the object

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
 
 const prisma = new PrismaClient({
 	log: ['query'],
@@ -15,10 +16,20 @@ async function bootstrap() {
 		origin: true, // Allow access from any app
 	})
 
-	fastify.get('/pools/count', async () => {
-		const count = await prisma.pool.count()
+	fastify.post('/pools', async (request, reply) => {
+		const createPoolBody = z.object({
+			title: z.string(),
+		})
+		const { title } = createPoolBody.parse(request.body)
 
-		return { count }
+		await prisma.pool.create({
+			data: {
+				title,
+				code: '12$#@1',
+			},
+		})
+
+		return reply.status(201).send({ title })
 	})
 
 	await fastify.listen({ port: 5000, host: '0.0.0.0' })

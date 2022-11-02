f0185af3eb7b48a2450b64bb28410f2f6d33464d
Ronaldo <ronaldovctor@outlook.com>
2022-11-02 10:49:09 -0300

    mobile: Signin screen finished

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -5,12 +5,17 @@ import { Button } from '../components/Button'
 
 export function SignIn() {
 	return (
-		<Center flex={1} bgColor="gray.900" alignItems="center" justifyContent="center">
+		<Center flex={1} bgColor="gray.900" p={7}>
 			<Logo width={212} height={40} />
 			<Button
 				title="Entrar com o Google"
 				leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+				mt={12}
 			/>
+			<Text color="white" textAlign="center" mt={4}>
+				Não utilizamos nenhuma informação além {'\n'} do seu e-mailpara criação de sua
+				conta.
+			</Text>
 		</Center>
 	)
 }

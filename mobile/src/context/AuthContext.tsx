12499e00a628ae7c88bdf2a3ceb68b0b6aa4457e
Ronaldo <ronaldovctor@outlook.com>
2022-11-02 12:34:13 -0300

    mobile: initial auth structure

--- mobile/src/context/AuthContext.tsx
+++ mobile/src/context/AuthContext.tsx
@@ -1,4 +1,10 @@
-import { createContext, ReactNode } from 'react'
+import { createContext, ReactNode, useState, useEffect } from 'react'
+import * as Google from 'expo-auth-session/providers/google'
+import * as AuthSession from 'expo-auth-session'
+import * as WebBrowser from 'expo-web-browser'
+import { consts } from '../../consts'
+
+WebBrowser.maybeCompleteAuthSession()
 
 export interface UserProps {
 	name: string
@@ -7,6 +13,7 @@ export interface UserProps {
 
 export interface AuthContextDataProps {
 	user: UserProps
+	isUserLoading: boolean
 	signIn: () => Promise<void>
 }
 
@@ -17,15 +24,43 @@ interface AuthProviderProps {
 export const AuthContext = createContext({} as AuthContextDataProps)
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
+	const [user, setUser] = useState<UserProps>({} as UserProps)
+	const [isUserLoading, setIsUserLoading] = useState(false)
+
+	const [request, response, promptAsync] = Google.useAuthRequest({
+		clientId: consts.CLIENT_ID,
+		redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+		scopes: ['profile', 'email'],
+	})
+
 	async function signIn() {
-		console.log('vamos logar')
+		try {
+			setIsUserLoading(true)
+			await promptAsync()
+		} catch (error) {
+			console.log(error)
+			throw error
+		} finally {
+			setIsUserLoading(false)
+		}
 	}
+
+	async function signInWithGoogle(access_token: string) {
+		console.log('Token', access_token)
+	}
+
+	useEffect(() => {
+		if (response?.type === 'success' && response.authentication?.accessToken) {
+			signInWithGoogle(response.authentication.accessToken)
+		}
+	}, [response])
 
 	return (
 		<AuthContext.Provider
 			value={{
 				signIn,
-				user: { name: 'Ronaldo', avatarUrl: 'https://github.com/ronaldovctor.png' },
+				isUserLoading,
+				user,
 			}}
 		>
 			{children}

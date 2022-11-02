b9394d1ddb34554c5cd436e2d9a0013bab5f5af9
Ronaldo <ronaldovctor@outlook.com>
2022-11-02 10:43:55 -0300

    mobile: create button component

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,10 +1,16 @@
-import { Center, Text } from 'native-base'
+import { Center, Text, Icon } from 'native-base'
+import { Fontisto } from '@expo/vector-icons'
 import Logo from '../assets/logo.svg'
+import { Button } from '../components/Button'
 
 export function SignIn() {
 	return (
 		<Center flex={1} bgColor="gray.900" alignItems="center" justifyContent="center">
 			<Logo width={212} height={40} />
+			<Button
+				title="Entrar com o Google"
+				leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+			/>
 		</Center>
 	)
 }

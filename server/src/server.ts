ef004c4aab05e779ed7e6422260e83b1d0bf72bf
Ronaldo <ronaldovctor@outlook.com>
2022-11-03 09:23:48 -0300

    api: refactoring routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,8 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
-import { z } from 'zod'
-
-const prisma = new PrismaClient({
-	log: ['query'],
-})
+import { poolRoutes } from './routes/pool'
+import { guessRoutes } from './routes/guess'
+import { userRoutes } from './routes/user'
 
 async function bootstrap() {
 	const fastify = Fastify({
@@ -16,21 +13,9 @@ async function bootstrap() {
 		origin: true, // Allow access from any app
 	})
 
-	fastify.post('/pools', async (request, reply) => {
-		const createPoolBody = z.object({
-			title: z.string(),
-		})
-		const { title } = createPoolBody.parse(request.body)
-
-		await prisma.pool.create({
-			data: {
-				title,
-				code: '12$#@1',
-			},
-		})
-
-		return reply.status(201).send({ title })
-	})
+	fastify.register(poolRoutes)
+	fastify.register(guessRoutes)
+	fastify.register(userRoutes)
 
 	await fastify.listen({ port: 5000, host: '0.0.0.0' })
 }
